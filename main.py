2d4304f5a059a035de76073de57b14c81234c0ca
Tim Wyse <wysetm@gmail.com>
2025-08-15 11:14:34 +0100

    add try/except for handling trade

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from player import NANO, MINI, FOUR_1, FOUR_0, HUMAN, LLAMA_3_3B
 CONFIG = GameConfig(
     players=[HUMAN, LLAMA_3_3B, FOUR_1],
     surplus=DEFAULT_SURPLUS,
-    grid_size=5,
+    grid_size=2,
     resource_mode='single_type_each',
     # temperature=DEFAULT_TEMPERATURE,
     # random_start_block_size=1,
     # random_goal_block_size=1,
-    # colors=[c for c in COLOR_MAP if c != 'BK'][:3],
-    # grid=None
+    colors=['R', 'G', 'B'],
+    grid=[['R', 'R'], ['B', 'R']]
 )
 
 if __name__ == "__main__":
