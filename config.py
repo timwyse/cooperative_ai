f49455076683a31aa7f75655e7457114e9647eb7
Tim Wyse <wysetm@gmail.com>
2025-08-15 12:05:27 +0100

    improve default value handling

--- config.py
+++ config.py
@@ -3,26 +3,31 @@ from dataclasses import dataclass, field
 from typing import List, Optional, TYPE_CHECKING
 from constants import COLOR_MAP
 
-if TYPE_CHECKING:
-    from player import Agent  
 
 @dataclass
 class GameConfig:
     """
-    Stores configuration settings shared between the Game and Player classes.
-    Acts as a central source of truth for game parameters.
+    Stores configuration settings shared between the Game and Player classes
+    and sets the set the default parameters for the game. 
     """
-    players: List[Agent]  # Agent can be forward-referenced now
-    surplus: float
-    grid_size: int
-    resource_mode: str = 'single_type_each'
+    players: Optional[List[str]] = None
+    surplus: float = 1.5 # this is a multiplier of the minimum number of steps required to complete the game.
+    grid_size: int = 3
+    resource_mode: str = 'single_type_each' # currently only 'single_type_each' is supported, where each player has a single type of resource
     temperature: float = 1.0
-    random_start_block_size: int = 1
-    random_goal_block_size: int = 1
+    random_start_block_size: int = 1 # this is the size of the top left corner within which the players start
+    random_goal_block_size: int = 1 # this is the size of the bottom right corner within which the players' goals are located
     colors: List[str] = field(default_factory=list)
     grid: Optional[List[List[str]]] = None
 
     def __post_init__(self):
-            # Dynamically set colors based on the number of players if not provided
-            if not self.colors:
-                self.colors = [c for c in COLOR_MAP if c != 'BK'][:len(self.players)]
+        if self.players is None:
+            from player import HUMAN  
+            self.players = [HUMAN, HUMAN]
+        if not self.colors:
+            self.colors = [
+                c for c in COLOR_MAP if c != 'BK'
+            ][:len(self.players)]
+        
+# Base configuration used by Game unless overridden in main.py
+DEFAULT_CONFIG = GameConfig()
