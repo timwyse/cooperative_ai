2b367d197b5194c6cecc7582f8648ce63586cebf
Tim Wyse <wysetm@gmail.com>
2025-08-27 13:46:59 +0100

    allow to load saved config files; agents in agents.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from dataclasses import replace
 
-from config import DEFAULT_CONFIG, GameConfig
+from config import DEFAULT_CONFIG, GameConfig, load_config
 from game import Game
 from logger import GameLogger
-from player import NANO, MINI, FOUR_1, FOUR_0, HUMAN, LLAMA_3_3B
+# from player import NANO, MINI, FOUR_1, FOUR_0, HUMAN, LLAMA_3_3B, QWEN_2_7B, DEEPSEEK
+from agents import *
 
 
 # refer to config.py to see which arguments are available for the game and what their defaults are
 # can replace any of the default values in DEFAULT_CONFIG with your own values
 CONFIG = replace(
     DEFAULT_CONFIG,
-    players=[LLAMA_3_3B, LLAMA_3_3B],
-    grid_size=5,
-    # colors=['R', 'B'],
+    players=[QWEN_2_7B, QWEN_2_7B],
+    grid_size=3,
+    colors=['R', 'B'],
     # resource_mode='manual',
-    # grid=[['R', 'R', 'R'], ['G', 'R', 'G'], ['R','G', 'R']],
+    grid=[['R', 'B', 'R'], ['R', 'B', 'R'], ['R','B', 'B']],
     # manual_resources = [{'R':4, 'B':1, 'G':1}, {'R':2, 'B':2, 'G': 2}]
 )
+
+# Alternatively, load a config from a JSON or YAML file
+# CONFIG = load_config("configs/full_info_trade_needed.yaml")
+
+
 
 # set LOGGER = GameLogger() to enable logging to a file, or None to disable logging
 LOGGER = GameLogger()
 
 if __name__ == "__main__":
-    
+    print(f"""
+          ******
+
+          Starting new Game with parameters: {CONFIG}
+
+          ******
+          """)
     Game(config=CONFIG, logger=LOGGER).run()
--- player.py
+++ player.py
@@ -1,11 +1,10 @@
-from collections import defaultdict, deque, namedtuple, Counter
+from collections import defaultdict, deque, Counter
 import copy
 import json
 import random
 import re
 
 from openai import OpenAI
-from tabulate import tabulate
 from together import Together
 
 from config import GameConfig
@@ -14,18 +13,6 @@ from grid import Grid
 
 
 
-### AGENTS - add more below as needed
-# https://openai.com/api/pricing/
-# https://api.together.ai/models
-Agent = namedtuple("Agent", ["name", "value", "api"])
-HUMAN = Agent(name="human", value=None, api=None)
-NANO = Agent(name="4.1 nano", value="gpt-4.1-nano-2025-04-14", api='open_ai') # $0.40 per million output tokens
-MINI = Agent(name="4.1 mini", value="gpt-4.1-mini", api='open_ai') # $1.60 per million output tokens
-FOUR_1 = Agent(name="4.1", value="gpt-4.1", api='open_ai') # $8 per million output tokens
-FOUR_0 = Agent(name="4o", value="gpt-4o", api='open_ai') # $10 per million output tokens
-DEEPSEEK = Agent(name="DeepSeek_R1", value="deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free", api='together') # free, but slow/ limited
-QWEN_2_7B = Agent(name="QWEN_25_7B", value="Qwen/Qwen2.5-7B-Instruct-Turbo", api='together') # $0.30 per million output tokens
-LLAMA_3_3B = Agent(name="Llama_3_3B", value="meta-llama/Llama-3.2-3B-Instruct-Turbo", api='together')# $0.06 per output million tokens
 
 
 DEFAULT_SYSTEM_PROMPT = """
