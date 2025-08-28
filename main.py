29198a59135e75eeafb3a35c94779cfd1c35fe6e
Tim Wyse <wysetm@gmail.com>
2025-08-28 16:31:39 +0100

    fix for manual resources

--- main.py
+++ main.py
@@ -9,29 +9,26 @@ from agents import *
 # refer to config.py to see which arguments are available for the game and what their defaults are
 # can replace any of the default values in DEFAULT_CONFIG with your own values
 CONFIG = replace(
-    DEFAULT_CONFIG,
-    # players=[QWEN_2_7B, QWEN_2_7B],
+    DEFAULT_CONFIG, # can use DEFAULT_CONFIG or load a specific configs here eg load_config("configs/fitn_3.yaml"),
+    players=[LLAMA_3_3B, LLAMA_3_3B],
     grid_size=5,
-    colors=['R', 'B', 'G'],
+    colors=['R', 'B', 'G', 'Y'],
     resource_mode='manual',
-    grid=[['G', 'G', 'B', 'B', 'B'],
-          ['G', 'R', 'R', 'R', 'R'],
-          ['B', 'R', 'R', 'R', 'R'],
-          ['B', 'R', 'R', 'R', 'B'],
-          ['B', 'R', 'R', 'B', 'B'], 
+    grid=[['G', 'B', 'R', 'B', 'R'],
+          ['R', 'Y', 'B', 'R', 'B'],
+          ['B', 'R', 'Y', 'B', 'R'],
+          ['R', 'B', 'R', 'Y', 'B'],
+          ['B', 'R', 'B', 'R', 'G'],
           ],
     manual_resources = [{'R':20, 'G': 4}, {'B':20, 'G': 4}],
-#     manual_start_positions = [(0,0), (0,4)],
-#     manual_goal_positions = [(4,4), (4,0)],
+# manual_goal_positions=[(3, 4), (3, 4)],
 )
 
-# Alternatively, load a config from a JSON or YAML file
-# CONFIG = load_config("configs/fitn_3.yaml")
 
 
 
 # set LOGGER = GameLogger() to enable logging to a file, or None to disable logging
-LOGGER = None
+LOGGER = GameLogger()
 
 if __name__ == "__main__":
     print(f"""
