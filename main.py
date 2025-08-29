62a576f238621c5916d89ee7980d5aad697ea6f8
Tim Wyse <wysetm@gmail.com>
2025-08-29 18:22:30 +0100

    add game configs

--- main.py
+++ main.py
@@ -9,22 +9,21 @@ from agents import *
 # refer to config.py to see which arguments are available for the game and what their defaults are
 # can replace any of the default values in DEFAULT_CONFIG with your own values
 CONFIG = replace(
-    DEFAULT_CONFIG, # can use DEFAULT_CONFIG or load a specific configs here eg load_config("configs/fitn_3.yaml"),
-    players=[LLAMA_3_3B, LLAMA_3_3B],
-    grid_size=5,
-    colors=['R', 'B', 'G', 'Y'],
-    resource_mode='manual',
-    grid=[['G', 'B', 'R', 'B', 'R'],
-          ['R', 'Y', 'B', 'R', 'B'],
-          ['B', 'R', 'Y', 'B', 'R'],
-          ['R', 'B', 'R', 'Y', 'B'],
-          ['B', 'R', 'B', 'R', 'G'],
-          ],
-    manual_resources = [{'R':20, 'G': 4}, {'B':20, 'G': 4}],
-# manual_goal_positions=[(3, 4), (3, 4)],
+    DEFAULT_CONFIG, # can use DEFAULT_CONFIG or load a specific configs here eg load_config("configs/simple_prisoners_dilemma.yaml"),
+#     players=[LLAMA_3_3B, LLAMA_3_3B],
+#     grid_size=5,
+#     colors=['R', 'B', 'G', 'Y', 'PK'],
+#     resource_mode='manual',
+#     grid=[['G', 'R', 'R', 'R', 'R'],
+#           ['B', 'PK','PK','PK', 'R'],
+#           ['B', 'PK','PK', 'PK', 'R'],
+#           ['B', 'PK','PK', 'PK', 'B'],
+#           ['B', 'B', 'B', 'R', 'G'],
+#           ],
+#     manual_resources = [{'G':20, 'B': 4}, {'G':20, 'R': 4}],
+# #     manual_start_positions=[(0, 0), (0, 4)],
+# #     manual_goal_positions=[(4, 4), (4, 0)],
 )
-
-
 
 
 # set LOGGER = GameLogger() to enable logging to a file, or None to disable logging
